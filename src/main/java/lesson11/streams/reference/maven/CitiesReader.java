195081aa6d285e575fd7dee008e5f7c91070a753
Aleksandr Pimenov <aleksandrinstall@gmail.com>
2019-09-21 15:41:09 +0400

    Java.firstMaven - fix Lesson13

--- src/main/java/lesson11/streams/reference/maven/CitiesReader.java
+++ src/main/java/lesson11/streams/reference/maven/CitiesReader.java
@@ -3,48 +3,54 @@ package lesson11.streams.reference.maven;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static lesson11.streams.reference.maven.Lesson11.fileAddress;
+
 class CitiesReader {
-    void loader(Cites cityObj, String fileAddress) {
-        try (BufferedReader bfCityReader = new BufferedReader
-                (new FileReader(fileAddress))) {
+    private List<String> cityNames;
+    private List<String> citySectors;
+    private List<String> sectorNames;
+    private List<UUID> sectorIDs;
+    private List<Integer> sectorLengths;
+    private List<Integer> sectorWidths;
 
-            List<String> fileValues = bfCityReader.lines()
-                    .flatMap(s -> Stream.of(s.split(",")))
-                    .collect(Collectors.toList());
+    void loader(Cites citesObj, String fileAddress) {
+        try {
+            List<String> fileContent =
+                    new BufferedReader(new FileReader(fileAddress))
+                            .lines()
+                            .collect(Collectors.toList());
 
-            List<String> cityNames = stringCollector(fileValues, "City:");
-            List<String> citySectors = stringCollector(fileValues, "Class:");
-            List<String> sectorNames = stringCollector(fileValues, "Name:");
-            List<UUID> sectorIDs = idCollector(fileValues,"ID:");
-            List<Integer> sectorLengths = intCollector(fileValues,"Length:");
-            List<Integer> sectorWidths = intCollector(fileValues,"Width:");
-
-            for (int i = 0; i < cityNames.size(); i++) {
-                addSector(
-                        addCity(cityObj, cityNames.get(i)),
-                        citySectors.get(i),
-                        sectorNames.get(i),
-                        sectorIDs.get(i),
-                        sectorLengths.get(i),
-                        sectorWidths.get(i)
-                );
-            }
+            separation(fileContent);
+            distribution(citesObj);
 
         } catch (IOException exc) {
             System.out.println("File access error!");
         }
     }
 
+    private void separation(List<String> fileContent){
+        List<String> fileValues = fileContent.stream()
+                .flatMap(s -> Stream.of(s.split(",")))
+                .collect(Collectors.toList());
+
+        cityNames = stringCollector(fileValues, "City:");
+        citySectors = stringCollector(fileValues, "Class:");
+        sectorNames = stringCollector(fileValues, "Name:");
+        sectorIDs = idCollector(fileValues,"ID:");
+        sectorLengths = intCollector(fileValues,"Length:");
+        sectorWidths = intCollector(fileValues,"Width:");
+    }
     private List<String> stringCollector(List<String> fileValues, String type) {
         return fileValues.stream()
-                        .filter(str -> str.startsWith(type))
-                        .map(str -> str.substring(str.indexOf(':') + 1))
-                        .collect(Collectors.toList());
+                .filter(str -> str.startsWith(type))
+                .map(str -> str.substring(str.indexOf(':') + 1))
+                .collect(Collectors.toList());
     }
     private List<UUID> idCollector(List<String> fileValues, String type) {
         return fileValues.stream()
@@ -61,10 +67,18 @@ class CitiesReader {
                 .collect(Collectors.toList());
     }
 
-    private City addCity(Cites cites, String cityName) {
-        if (cites.isHere(cityName))return cites.getCity(cityName);
-        else cites.addCity(cityName,new City(cityName));
-        return cites.getCity(cityName);
+
+    private void distribution(Cites citesObj){
+        for (int i = 0; i < cityNames.size(); i++) {
+            addSector(
+                    addCity(citesObj, cityNames.get(i)),
+                    citySectors.get(i),
+                    sectorNames.get(i),
+                    sectorIDs.get(i),
+                    sectorLengths.get(i),
+                    sectorWidths.get(i)
+            );
+        }
     }
     private void addSector(City cityObj, String citySector, String sectorName,
                            UUID sectorID, int sectorLength, int sectorWidth) {
@@ -72,16 +86,19 @@ class CitiesReader {
             case "Park":
                 cityObj.addSector(new Park(sectorName,sectorID, sectorLength, sectorWidth));
                 break;
-
             case "Street":
                 cityObj.addSector(new Street(sectorName,sectorID, sectorLength, sectorWidth));
                 break;
-
             case "District":
                 cityObj.addSector(new District(sectorName,sectorID, sectorLength, sectorWidth));
                 break;
             default:
                 System.out.println("Sector not found!");
         }
+    }
+    private City addCity(Cites cites, String cityName) {
+        if (cites.isHere(cityName))return cites.getCity(cityName);
+        else cites.addCity(cityName,new City(cityName));
+        return cites.getCity(cityName);
     }
 }
